981067dd73f8303701e454944dbceb3347a0d46d
Minh Trần <117151487+minhtranlc@users.noreply.github.com>
2025-01-15 14:09:21 +0700

    feat(lp-token-generator): done module lpTokenGenerator (#11)

---
+++ pages/lp-token-logo-generator/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import LpTokenLogoGenerator from 'src/lp-token-logo-generator/LPTokenLogoGenerator';
+
+const LPTokenLogoGeneratorPage: NextPage = () => {
+  return <LpTokenLogoGenerator />;
+};
+
+export default LPTokenLogoGeneratorPage;
